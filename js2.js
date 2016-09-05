f93a94b12ac3263018b14c006d70e830d46b265a
iqbalbuldani <airohmah017@gmail.com>
2016-09-05 12:18:14 +0700

    Add files via upload

---
+++ js2.js
@@ -0,0 +1,9 @@
+<div style="text-align: left;">
+<b:if cond="data:blog.pageType == &quot;item&quot;">
+</b:if>
+<script src="https://cdn.rawgit.com/iqbalbuldani/safelink/master/safelink.js" type="text/javascript"></script> <script type="text/javascript">
+    var currentURL=location.href;
+    var str = currentURL;
+    var res = str.replace("http://download.kuncigitik.com/2016/08/rapid-prototyping-3d-printing.html?url=", "");
+    document.write('<button id="download" class="Visit_Link" onclick="changeLink();" style="font-size: x-small; font-weight: normal;">download</button>');onload:generate();//]]></script>
+</div>
